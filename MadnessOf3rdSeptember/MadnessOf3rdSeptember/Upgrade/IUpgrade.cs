50651e2f275177971eaa7b9539f2e3a278fc5bb6
Hokku <darksouls2make@gmail.com>
2023-08-17 22:43:11 +0400

    Create IUpgrade Interface

---
+++ MadnessOf3rdSeptember/MadnessOf3rdSeptember/Upgrade/IUpgrade.cs
@@ -0,0 +1,12 @@
+﻿namespace MadnessOf3rdSeptember.Upgrade;
+
+public interface IUpgrade
+{
+    public string Name { get; }
+    public int CurrLevel { get; set; }
+    public int MaxLevel { get; }
+    public int StartCost { get; }
+    public int CurrentCostToUpgrade { get; set; }
+    public int DealByLevel { get; set; }
+    public int CostToUpgradeLevel();
+}
