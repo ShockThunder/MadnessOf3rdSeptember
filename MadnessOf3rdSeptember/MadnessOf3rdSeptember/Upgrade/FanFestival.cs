7602c153e2c6d02a3775f77db2741b24662f3245
Hokku <darksouls2make@gmail.com>
2023-09-01 21:53:41 +0400

    Added FanFestival class

---
+++ MadnessOf3rdSeptember/MadnessOf3rdSeptember/Upgrade/FanFestival.cs
@@ -0,0 +1,33 @@
+﻿namespace MadnessOf3rdSeptember.Upgrade;
+
+public class FanFestival : IUpgrade
+{
+    public string Name { get; }
+    public string Image { get; set; }
+    public int CurrentLevel { get; set; }
+    public int MaxLevel { get; }
+    public int StartCost { get; }
+    public int CurrentCost { get; set; }
+    public int CountByLevel { get; set; }
+
+    public FanFestival()
+    {
+        Name = "FanFestival";
+        CurrentLevel = 0;
+        MaxLevel = 10;
+        StartCost = 500;
+        CurrentCost = StartCost;
+        CountByLevel = 200;
+    }
+    public long Buy(long currentCount)
+    {
+        if(currentCount < CurrentCost)
+            return currentCount;
+
+        CurrentLevel++;
+        currentCount -= CurrentCost;
+        CurrentCost = (CurrentCost + StartCost) * (200 / 150) * CurrentLevel;
+
+        return currentCount;
+    }
+}
