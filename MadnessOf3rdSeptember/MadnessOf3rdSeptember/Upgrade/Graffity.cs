2c824b109d431f681e293a1634d4cd18a717e338
Timur Sidoriuk <skysidtim@gmail.com>
2023-09-03 00:20:10 +0200

    fix coefficients, rework upgrades, fix 0.00000000000000003 error

--- MadnessOf3rdSeptember/MadnessOf3rdSeptember/Upgrade/Graffity.cs
+++ MadnessOf3rdSeptember/MadnessOf3rdSeptember/Upgrade/Graffity.cs
@@ -1,31 +1,15 @@
 ﻿namespace MadnessOf3rdSeptember.Upgrade;
 
-public class Graffity : IUpgrade
+public class Graffity : Upgrade
 {
-    public string Name { get; }
-    public string Description { get; set; }
-    public int CurrentLevel { get; set; }
-    public int MaxLevel { get; }
-    public int StartCost { get; }
-    public int CurrentCost { get; set; }
-    public int CountByLevel { get; set; }
-
     public Graffity()
     {
         Name = "Graffity";
         CurrentLevel = 0;
-        MaxLevel = 10;
-        StartCost = 50;
+        StartCost = 12000;
         CurrentCost = StartCost;
-        CountByLevel = 25;
+        CountByLevel = 47;
         Description =
             "Стрит-арт граффити: Рисуйте граффити с портретами Шуфутинского на стенах, чтобы привлечь больше фанатов и повысить эффективность кликов.";
-    }
-
-    public void Buy()
-    {
-        CurrentLevel++;
-        CountByLevel += 5 + CurrentCost / 100;
-        CurrentCost = ((int)Math.Pow(StartCost, 3) / 30) * (CurrentLevel + 1);
     }
 }
--- MadnessOf3rdSeptember/MadnessOf3rdSeptember/Upgrade/Space.cs
+++ MadnessOf3rdSeptember/MadnessOf3rdSeptember/Upgrade/Space.cs
@@ -1,29 +1,15 @@
 ﻿namespace MadnessOf3rdSeptember.Upgrade;
 
-public class Space : IUpgrade
+public class Space : Upgrade
 {
-    public string Name { get; }
-    public string Description { get; set; }
-    public int CurrentLevel { get; set; }
-    public int MaxLevel { get; }
-    public int StartCost { get; }
-    public int CurrentCost { get; set; }
-    public int CountByLevel { get; set; }
-
     public Space()
     {
         Name = "Space";
         CurrentLevel = 0;
-        MaxLevel = 1;
-        StartCost = 100000000;
+        StartCost = 1000000000000;
         CurrentCost = StartCost;
-        CountByLevel = 133333;
+        CountByLevel = 10000000;
         Description =
             "Космическая ракета 'В будущее с Шуфутинским': Отправьтесь в космическое путешествие с Шуфутинским, чтобы найти новые даты и события, которые можно перевернуть.";
-    }
-
-    public void Buy()
-    {
-        CurrentLevel++;
     }
 }
