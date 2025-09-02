9ba15f9d7f5aa879afcf721199d6fc3c5aa93827
Timur Sidoriuk <skysidtim@gmail.com>
2025-09-03 01:05:47 +0300

    Update RecordStudio.cs

--- MadnessOf3rdSeptember/MadnessOf3rdSeptember/Upgrade/RecordStudio.cs
+++ MadnessOf3rdSeptember/MadnessOf3rdSeptember/Upgrade/RecordStudio.cs
@@ -8,8 +8,9 @@ public class RecordStudio : Upgrade
         CurrentLevel = 0;
         StartCost = 75000000000;
         CurrentCost = StartCost;
-        CountByLevel = 1600000;
+        CountByLevel = 10000000;
         Description =
             "Шуфутинская студия записи: Записывай новые песни Шуфутинского, чтобы получать монеты и ускорять процесс переворота календаря.";
     }
+
 }
