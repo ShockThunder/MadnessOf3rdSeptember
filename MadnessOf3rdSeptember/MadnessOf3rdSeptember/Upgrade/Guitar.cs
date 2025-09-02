f777b40855e5c2cf6282e5953510a523d4fe82df
Timur Sidoriuk <skysidtim@gmail.com>
2025-09-03 01:09:54 +0300

    Update Guitar.cs

--- MadnessOf3rdSeptember/MadnessOf3rdSeptember/Upgrade/Guitar.cs
+++ MadnessOf3rdSeptember/MadnessOf3rdSeptember/Upgrade/Guitar.cs
@@ -8,7 +8,7 @@ public class Guitar : Upgrade
         CurrentLevel = 0;
         StartCost = 15;
         CurrentCost = StartCost;
-        CountByLevel = 0.1;
+        CountByLevel = 1;
         Description =
             "Увеличенная гитара Шуфутинского - увеличивает размер гитары Шуфутинского в два раза, делая каждый аккорд более мелодичным и эффективным для переворотов календаря. Ваши клики звучат громче и ярче!";
     }
